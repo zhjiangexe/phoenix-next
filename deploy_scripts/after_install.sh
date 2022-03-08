a52c6498984ac410df46b50cbbdf9941f7b1b8b3
jiang <jiang.jiang@eton.tw>
2022-03-08 17:09:10 +0800

    fix: appspec.yml test

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo $(whoamil)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export HOME="/home/ec2-user/"
 cd /home/ec2-user/my-app
 npm install
 #echo installing pm2
--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo installing application with npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 16.13.0
 cd /home/ec2-user/my-app
+which node
+which npm
