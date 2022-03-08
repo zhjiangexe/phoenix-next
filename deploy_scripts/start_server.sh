3afe6e456e31663b68d0aabaef981f7cc56fcf79
jiang <jiang.jiang@eton.tw>
2022-03-08 17:51:45 +0800

    fix: start_server.sh add npm install -g next

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -1,7 +1,7 @@
 echo "app start ;)"
 source /home/ec2-user/.bash_profile
-
 cd /home/ec2-user/my-app
+npm install -g next
 npm install
 npm run build
 npm run start > /dev/null 2> /dev/null < /dev/null &
