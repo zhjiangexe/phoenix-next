90963407ae03bc208d121f6ee5893f95ae2971dd
jiang <jiang.jiang@eton.tw>
2022-03-08 16:24:48 +0800

    fix: appspec.yml modify bash

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo installing application with npm
 cd /home/ec2-user/my-app
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
