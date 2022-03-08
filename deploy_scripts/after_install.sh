f7ce671033ffb6d52f942913df29a1fafa6bc3d9
jiang <jiang.jiang@eton.tw>
2022-03-08 15:58:13 +0800

    fix: appspec.yml modify bash

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/my-app
 npm install
-npm install pm2 -g
 #echo installing pm2
 #sudo npm install pm2 -g
--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo installing application with npm
 cd /home/ec2-user/my-app
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-yum -y install nodejs npm
