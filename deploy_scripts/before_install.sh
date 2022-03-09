b5a90d081b045bbbe85f9244559955c092844d2a
jiang <jiang.jiang@eton.tw>
2022-03-10 01:13:13 +0800

    fix: appspec.yml file_exists_behavior

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,13 +1,7 @@
 source /home/ec2-user/.bash_profile
-killall node
-rm -rf /home/ec2-user/my-app
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16.13.0
 node -v
-sudo rm -rf node_modules
-sudo mkdir node_modules
-chmod -R 777 node_modules
-sudo rm -rf package-lock.json
