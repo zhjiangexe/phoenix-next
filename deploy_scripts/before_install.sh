8bbc7d7a021cf9fde968557403f708e628ade94d
jiang <jiang.jiang@eton.tw>
2022-03-10 02:46:01 +0800

    fix: before_install.sh

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,3 +1,8 @@
 source /home/ec2-user/.bash_profile
 each 'node -v'
 node -v
+killall node
+rm -rf /home/ec2-user/my-app
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 16.13.0
