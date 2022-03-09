e3f6950633f63aca343067689cc6c8c5ccbeacad
jiang <jiang.jiang@eton.tw>
2022-03-10 02:59:24 +0800

    fix: before_install.sh

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -2,6 +2,8 @@ echo "app start ;)"
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/my-app
 #npm install -g next
+sudo mkdir node_modules
+sudo chmod -R 777 node_modules
 npm install
 #npm run build
 npm run start > /dev/null 2> /dev/null < /dev/null &
